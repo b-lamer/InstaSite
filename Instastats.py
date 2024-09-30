f40168c55a838096b2d36315e8e1887e549fdf29
b-lamer <152201467+b-lamer@users.noreply.github.com>
2024-09-30 13:23:19 -0500

    Update Instastats.py

--- Instastats.py
+++ Instastats.py
@@ -1,7 +1,6 @@
 import instaloader
 from config import logins
 
-userquery = ""
 #Creates instance of instaloader
 L = instaloader.Instaloader()
 
@@ -9,11 +8,13 @@ L = instaloader.Instaloader()
 L.login(logins['username'], logins['password'])
 
 #Loads profile
-while userquery != "quit":
+while True:
     follower_list = []
     following_list = []
 
     userquery = input("Enter the instagram username you'd like to search: ")
+    if userquery == "quit":
+        break
 
     profile = instaloader.Profile.from_username(L.context, userquery)
 
@@ -21,9 +22,20 @@ while userquery != "quit":
     followers = profile.get_followers()
     for follower in followers:
         follower_list.append(follower.username)
-    print(follower_list)
+    #print(follower_list)
+    #print ("\n")
 
     followings = profile.get_followees()
     for following in followings:
         following_list.append(following.username)
-    print(following_list)
+    #print(following_list)
+    #print("\n" + "-----------------------------------" + "\n")
+
+    print("These users are not following " + userquery + " back: ")
+    badfriends = []
+    for following in following_list:
+        if following not in follower_list:
+            badfriends.append(following)
+    if badfriends == []:
+        badfriends.append("None")
+    print(badfriends)
